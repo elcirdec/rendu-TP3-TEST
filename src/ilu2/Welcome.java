ddf018fb1206e3648b886c6efa42c0c4298f9073
elcirdec <114509438+elcirdec@users.noreply.github.com>
2023-05-02 15:20:11 +0200

    ITER_7

--- src/ilu2/Welcome.java
+++ src/ilu2/Welcome.java
@@ -3,29 +3,62 @@ package ilu2;
 public class Welcome {
 	
 	public static StringBuilder plusDeDeuxNomInput(String[] names,StringBuilder resultat) {
-		for (String name : names) {
+		for (int i=0;i<names.length-1;i++) {
 			resultat.append(", ");
-			resultat.append(name.substring(0, 1).toUpperCase() + name.substring(1));
+			resultat.append(names[i].substring(0, 1).toUpperCase() + names[i].substring(1));
 		}
+		resultat.append(" and ");
+		resultat.append(names[names.length-1].substring(0, 1).toUpperCase() + names[names.length-1].substring(1));
 		return resultat;
 	}
 
-	public static StringBuilder crisAvecPlusieursNoms(String[] names,StringBuilder resultat) {
+	public static StringBuilder motsMinuscule(String[] names,StringBuilder resultat,int nbMotsMinuscule) {
+		int occMin=0;
 		for (String name : names) {
 			if(!name.equals(name.toUpperCase())) {
-				resultat.append(", ");
+				if(occMin==nbMotsMinuscule-1 && occMin!=0) {
+					resultat.append(" and ");
+				}else {
+					resultat.append(", ");
+				}						
+				occMin++;
 				resultat.append(name.substring(0, 1).toUpperCase() + name.substring(1));
 			}
 		}
+		return resultat;
+	}
+	public static StringBuilder motsMajuscule(String[] names,StringBuilder resultat, int nbMotsMajuscule) {
+		int occMaj=0;
+		System.out.println(nbMotsMajuscule);
 		resultat.append(". AND HELLO");
 		for (String name : names) {
 			if(name.equals(name.toUpperCase())) {
-				resultat.append(", ");
+				if(occMaj==nbMotsMajuscule-1 && occMaj!=0) {
+					resultat.append(" AND ");
+				}else {
+					resultat.append(", ");
+				}		
+				occMaj++;
 				resultat.append(name.substring(0, 1).toUpperCase() + name.substring(1));
-
 			}
 		}
 		resultat.append(" !");
+		return resultat;
+	}
+	
+	public static StringBuilder crisAvecPlusieursNoms(String[] names,StringBuilder resultat) {
+		int nbMotEnMin=0;
+		int nbMotEnMaj=0;
+		for (String i : names) {				
+			if(i.equals(i.toUpperCase())) {
+				nbMotEnMaj++;					
+			}else {
+				nbMotEnMin++;
+			}
+		}
+		resultat=motsMinuscule(names, resultat,nbMotEnMin);
+		resultat=motsMajuscule(names, resultat,nbMotEnMaj);		
+		System.out.println(resultat.toString());
 		return resultat;
 	}
 
@@ -40,7 +73,7 @@ public class Welcome {
 		if(names.length>=2 && isNameMaj) {
 			resultat=crisAvecPlusieursNoms(names, resultat);
 		}else if(names.length==2) {
-			resultat.append(", "+names[0].substring(0, 1).toUpperCase() + names[0].substring(1)+", "+names[1].substring(0, 1).toUpperCase() + names[1].substring(1));
+			resultat.append(", "+names[0].substring(0, 1).toUpperCase() + names[0].substring(1)+" and "+names[1].substring(0, 1).toUpperCase() + names[1].substring(1));
 		}else if(names.length>2) {
 			resultat=plusDeDeuxNomInput(names,resultat);
 		}
--- test/ilu2/WelcomeTest.java
+++ test/ilu2/WelcomeTest.java
@@ -27,16 +27,21 @@ class WelcomeTest {
 	
 	@Test
 	void testInput2Names() {
-		assertEquals(Welcome.welcome("amy,bob"), "Hello, Amy, Bob");
+		assertEquals(Welcome.welcome("amy,bob"), "Hello, Amy and Bob");
 	}
 
 	@Test
 	void testInputMoreThan2Names() {
-		assertEquals(Welcome.welcome("Amy, bob, jerry"), "Hello, Amy, Bob, Jerry");
+		assertEquals(Welcome.welcome("Amy, bob, jerry"), "Hello, Amy, Bob and Jerry");
 	}
 	
 	@Test
 	void testCrisAvecPlusieursNom() {
-		assertEquals(Welcome.welcome("Amy, BOB, Jerry"), "Hello, Amy, Jerry. AND HELLO, BOB !");
+		assertEquals(Welcome.welcome("Amy, BOB, Jerry"), "Hello, Amy and Jerry. AND HELLO, BOB !");
+	}
+	
+	@Test
+	void testAndEntreLesDerniersNoms() {
+		assertEquals(Welcome.welcome("bob, AMY, jerry, JACK"), "Hello, Bob and Jerry. AND HELLO, AMY AND JACK !");
 	}
 }
